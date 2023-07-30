af7ddf12e9291d07f53dd3b60082d3f65e8f5adb
a67878813 <len888len888@gmail.com>
2023-07-30 10:43:18 +0800

    change bugs

--- substituteDDNS.py
+++ substituteDDNS.py
@@ -5,33 +5,42 @@ import os
 import sys
 from enum import Enum
 
-#print(sys.platform)
+# print(sys.platform)
 if "win" in sys.platform:
     hosts = r"c:\Windows\System32\drivers\etc\hosts"
-else :
+else:
     hosts = r"\etc\hosts"
 
 LOG = True
 log_file = r"uploaded.log"
-#default config_data ,enable when config.json isn't exist.
+# default config_data ,enable when config.json isn't exist.
 config_data = {
     'behavior': 'Behavior.SERVER',
     'ServerIp': "192.168.88.230", 'ServerPort': '8888',
     'current_ip': '222.222.222.222', 'ip_-1': '222.222.111.111',
     'change_domain_name': 'lib.com',
-    'lib.com': "222.222.222.222"
+    'lib.com': "222.222.222.222",
+    'readed_domain': "['lib.com',]"
 }
+
+
+def hello_message():
+    mes = r"\nsubstituteDDNS 0.1 written by Esir at 2023.07 \n ALL COPYRITES RESERVED"
+
+
 class Behavior(Enum):
-    SERVER= 1
-    CLIENT_UP =2
-    CLIENT_GET =3
+    SERVER = 1
+    CLIENT_UP = 2
+    CLIENT_GET = 3
+
+
 def load_json():
     file_path = "config.json"
-    if len(sys.argv)>1:
+    if len(sys.argv) > 1:
         file_path = sys.argv[1]
-    with open(file_path,"r") as load_f:
+    with open(file_path, "r") as load_f:
         load_dic = json.load(load_f)
-        print("loaded: ", load_dic)
+        #print("loaded: ", load_dic)
     return load_dic
 
 
@@ -48,21 +57,20 @@ data_respond_PSOT = {
     'timestamp': '',
     'data': ''  # {'message_id': '25d55ad283aa400af464c76d713c07ad'}
 }
-return_false_data = {'error': 'has_error',}
-
+return_false_data = {'error': 'has_error', }
 
 
 def nowtimestr():
-    return time.strftime("%Y/%m/%d %H:%M:%S", (time.localtime()) )
-#server needs
-if my_behavior ==Behavior.SERVER:
+    return time.strftime("%Y/%m/%d %H:%M:%S", (time.localtime()))
+
+
+# server needs
+if my_behavior == Behavior.SERVER:
     from http.server import HTTPServer, BaseHTTPRequestHandler
     import json
     import shutil
     import io
     import urllib
-
-
 
     class Resquest(BaseHTTPRequestHandler):
         def do_GET(self):
@@ -79,7 +87,7 @@ if my_behavior ==Behavior.SERVER:
                     self.wfile.write(json.dumps(
                         {_path: config_data[_path]}).encode())
             else:
-                #send {error}
+                # send {error}
                 self.wfile.write(json.dumps(return_false_data).encode())
 
         def do_POST(self):
@@ -92,17 +100,24 @@ if my_behavior ==Behavior.SERVER:
             # print(_tempdict)
             # update dict
             if len(path) > 1:
-                _path = path[1:]
-                if _path in config_data.keys() & _path in config_data.keys():
-                    if _tempdict[_path] != config_data[_path]:
-                        config_data["ip_-1"] = config_data[_path]
-                        config_data[_path] = _tempdict[_path]  # update value
-                        print("update value at ", _path, " : ", config_data[_path])
-                        if LOG == True:
-                            with open(log_file,'a+') as f:
-                                f.write(nowtimestr()+ _path+"= "+ config_data[_path]+  "\n")
-                        # write to disk
-                        write_json(config_data)
+                C_FLAG = False
+                for posted_domain in _tempdict.keys():
+                    if posted_domain in config_data.keys():
+                        if _tempdict[posted_domain] != config_data[posted_domain]:
+                            C_FLAG = True
+                            config_data["ip_-1"] = config_data[posted_domain]
+                            # update value
+                            config_data[posted_domain] = _tempdict[posted_domain]
+                            print("update value at ", posted_domain,
+                                  " : ", config_data[posted_domain])
+                            if LOG == True:
+                                with open(log_file, 'a+') as f:
+                                    # log  domian = new ip
+                                    f.write(nowtimestr() + posted_domain +
+                                            "= " + config_data[posted_domain] + "\n")
+                if C_FLAG == True:
+                    # write to disk
+                    write_json(config_data)
 
             # update dict end
 
@@ -127,122 +142,164 @@ if my_behavior ==Behavior.SERVER:
             shutil.copyfileobj(f, self.wfile)
 
 
-
-
-def update_hosts(new_ip):
-    with open(hosts,'r') as f:
+def update_hosts(_new_dict):
+    """
+    call this func ONLY when there are changes.
+    """
+    with open(hosts, 'r', encoding='utf-8') as f:
         readed_host = f.readlines()
-    
 
-    # replace ip
-    #NEED_UPDQATE = False
     for i in range(len(readed_host)):
         if readed_host[i][0] != '#':
             this_line = readed_host[i].split()
-            if len(this_line) >0:
-                #this_line  
+            if len(this_line) > 1:  # at least 2 elem.
+                # this_line
                 # 0 ip   1 domain
-                if  (this_line[1] in config_data.keys()):
-                    print("replaced domain :",this_line[1]," to ip ", config_data[this_line[1]] , " with ", this_line[1] )
-                    this_line[0] = config_data[this_line[1] ]
+                if (this_line[1] in _new_dict.keys()):
+                    #                                         to ip wrong
+                    print("replaced domain :", this_line[1], " to ip ",
+                          _new_dict[this_line[1]], " with ", this_line[1])
+                    this_line[0] = _new_dict[this_line[1]]
                     readed_host[i] = " ".join(this_line)
 
     # save new hosts file
-    with open(hosts,'w') as f:
+    with open(hosts, 'w', encoding='utf-8') as f:
         f.writelines(readed_host)
 
+
 def my_getIP():
-    ip =requests.get(r'http://ifconfig.me/ip')
+    ip = requests.get(r'http://ifconfig.me/ip')
     return ip.text.strip()
+
+
+def my_get_server_inner_IP():
+    ip = requests.get(r'http://ifconfig.me/ip')
+    return ip.text.strip()
+
 
 def write_json(j_dic):
     file_path = "config.json"
-    if len(sys.argv)>1:
+    if len(sys.argv) > 1:
         file_path = sys.argv[1]
-    with open(file_path,"w") as f:
-        json.dump(j_dic,f)
-        print("written: ", j_dic )
+    with open(file_path, "w") as f:
+        json.dump(j_dic, f)
+        print("written: ", j_dic)
 
-#write_json(config_data)
+# write_json(config_data)
 
 
 def get_requrl():
     requrl = "http://" + config_data['ServerIp'] + ":" + config_data['ServerPort'] + "/"  \
-            + config_data['change_domain_name']
+        + config_data['change_domain_name']
     return requrl
 
 
-def get_server_innerIP():
-    response = requests.get(get_requrl())
+def get_server_innerIP(_domain_name='lib.com'):
+    #curr_ip = my_getIP()
+    requrl = "http://" + config_data['ServerIp'] + ":" + config_data['ServerPort'] + "/"  \
+        + _domain_name
+    response = requests.get(requrl)
+    print(response.json())
+
+
+def get_server_innerIP(_domain_name='lib.com'):
+    requrl = "http://" + config_data['ServerIp'] + ":" + config_data['ServerPort'] + "/"  \
+        + _domain_name
+    response = requests.get(requrl)
     r_data = json.loads(response.content.decode('UTF-8'))
-    print(r_data)
+    return r_data
 
-
-
-
-def post_server_innerIP():
-    pass
-    curr_ip = my_getIP()
-    if(config_data['current_ip']!= curr_ip):
+    if(config_data['current_ip'] != curr_ip):
         domain_name = config_data['change_domain_name']  # update local
 
-        #update server
-        response = requests.post(get_requrl(),json=json.dumps(  
-            {domain_name:curr_ip}
-                                                        ))
+        # update server
+        response = requests.post(get_requrl(), json=json.dumps(
+            {domain_name: curr_ip}
+        ))
         print(nowtimestr(), " updated IP:", curr_ip, " ", )
         temp_value = config_data['current_ip']
         config_data['ip_-1'] = temp_value
         config_data['current_ip'] = curr_ip
         write_json(config_data)
         if LOG == True:
-            with open(log_file,'a+') as f:
-                f.write(nowtimestr()+ " updated IP:"+ curr_ip+ "\n")
+            with open(log_file, 'a+') as f:
+                f.write(nowtimestr() + " updated IP:" + curr_ip + "\n")
 
 
+def post_server_innerIP():
+    pass
+    curr_ip = my_getIP()
+    if(config_data['current_ip'] != curr_ip):
+        domain_name = config_data['change_domain_name']  # update local
+
+        # update server
+        # server
+        response = requests.post(get_requrl(), json={domain_name: curr_ip}
+                                 )
+        print(nowtimestr(), " updated IP:", curr_ip, " ", )
+        # local
+        temp_value = config_data['current_ip']
+        config_data['ip_-1'] = temp_value
+        config_data['current_ip'] = curr_ip
+        write_json(config_data)
+        if LOG == True:
+            with open(log_file, 'a+') as f:
+                f.write(nowtimestr() + " updated IP:" + curr_ip + "\n")
 
 
 if __name__ == "__main__":
-    #check config.json, create new if not exist.
+    # check config.json, create new if not exist.
+    print(hello_message())
     if (not os.path.exists("config.json")):
-        #print('here')
+        # print('here')
         if my_behavior == Behavior.SERVER:
-            config_data['ServerIp']= my_getIP()
+            config_data['ServerIp'] = my_getIP()
         write_json(config_data)
     else:
-        #read json from config.json
-        #print('tt')
+        # read json from config.json
+        # print('tt')
         config_data = load_json()
         my_behavior = eval(config_data['behavior'])
 
     if my_behavior == Behavior.SERVER:
-        #server
+        # server
         pass
-        host = (config_data['ServerIp']  ,  int(config_data['ServerPort'])      )
-        server = HTTPServer(host , \
-                             Resquest)
+        host = (config_data['ServerIp'],  int(config_data['ServerPort']))
+        server = HTTPServer(host,
+                            Resquest)
         print("Starting server, listen at: %s:%s" % host)
         server.serve_forever()
 
     if my_behavior == Behavior.CLIENT_UP:
         # upload ip
+        print("Starting client ip, to host %s" % config_data['ServerIp'])
         pass
         while(1):
             post_server_innerIP()
             time.sleep(60)
-            
 
     if my_behavior == Behavior.CLIENT_GET:
-        #get ip
+        # get ip
         while(1):
+            _tempdict = get_server_innerIP(config_data['change_domain_name'])
+            C_FLAG = False
 
-            new_ = my_getIP()
-            if config_data[config_data['change_domain_name']] != new_:
+            # update config fire
+            for _domain in _tempdict.keys():
+                if _domain in config_data.keys():
+                    # check domin: ip changes
+                    if _tempdict[_domain] != config_data[_domain]:
+                        C_FLAG = True
+                        # update value
+                        config_data[_domain] = _tempdict[_domain]
+                        # print("update value at ", _domain,
+                        #         " : ", config_data[_domain])
 
-            # if ip changed 
-                #update local config_data
-                config_data[config_data['change_domain_name']] = new_
+            if C_FLAG == True:
+                # write to disk
+                write_json(config_data)
 
-                update_hosts(new_)
-                #update local
+                # update HOST file
+                update_hosts(_tempdict)
+
             time.sleep(60)
